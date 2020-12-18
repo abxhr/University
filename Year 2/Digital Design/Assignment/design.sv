d1a82625990bb521ec9b1591ffd647214d270805
abshar <absharaslam2001@gmail.com>
2020-12-18 23:19:31 +0400

    Update DD Assignment

--- Year 2/Digital Design/Assignment/design.sv
+++ Year 2/Digital Design/Assignment/design.sv
@@ -1,71 +1,22 @@
-// Code your design here
-// For check --> 1: Odd		2: Even
-
-module frequency_divider #(parameter CHECK = 1) (fin,reset,fout);
+module frequency_divider #(parameter W = 4,parameter N = 8)(fin,reset,fout);
 	output reg fout;
 	input fin;
 	input reset;
   
-  	odd_div U1(fin,reset,fout);
-  	even_div M1(fin,reset,fout);
-  
-    endmodule          
-
-
-module odd_div #(parameter WIDTH = 3,parameter N = 5)(fin,reset,fout);
-	input fin;
-	input reset;
-	output fout;
+  	reg [W-1:0] positive_c, negetive_c;
  
-	reg [WIDTH-1:0] pos_count, neg_count;
-	wire [WIDTH-1:0] r_nxt;
- 
-  always @(posedge fin)
+  	always @(posedge fin)
  		if (reset)
- 			pos_count <=0;
- 		else if (pos_count ==N-1) pos_count <= 0;
- 		else pos_count<= pos_count +1;
+ 			positive_c <=0;
+  		else  if (positive_c ==N-1) positive_c <= 0;
+ 		else positive_c<= positive_c +1; 
  
-  always @(negedge fin)
+  	always @(negedge fin)
  		if (reset)
- 			neg_count <=0;
- 		else  if (neg_count ==N-1) neg_count <= 0;
- 		else neg_count<= neg_count +1; 
- 
-	assign fout = ((pos_count > (N>>1)) | (neg_count > (N>>1))); 
-endmodule
- 
-          
-module even_div #(parameter N =6,parameter W = 3) (fin,reset,fout);
-  	input fin;
-	input reset;
-	output fout;
+ 			negetive_c <=0;
+  		else  if (negetive_c ==N-1) negetive_c <= 0;
+ 		else negetive_c<= negetive_c +1; 
   
-	reg [W-1:0] r_reg;
-	wire [W-1:0] r_nxt;
-	reg clk_track;
- 
-    always @(posedge fin or posedge reset)
-		begin
-          if (reset)
-     		begin
-        		r_reg <= 0;
-				clk_track <= 1'b0;
-     		end
- 
- 		  else if (r_nxt == N)
- 	   		begin
-	     		r_reg <= 0;
-	     		clk_track <= ~clk_track;
-	   		end
- 
-  		  else 
-            begin
-      			r_reg <= r_nxt;
-			end
-        end	
-        
- 	assign r_nxt = r_reg+1;   	      
- 	assign fout = clk_track;
+  assign fout = (N%2==0)?((positive_c<N/2)?1'b0:1'b1):((positive_c > (N>>1)) | (negetive_c > (N>>1)));
   
-	endmodule
+endmodule          
--- Year 2/Digital Design/Assignment/testbench.sv
+++ Year 2/Digital Design/Assignment/testbench.sv
@@ -1,15 +1,11 @@
-// Code your testbench here
-
-// Write a Verilog code and testbench to implement a frequency divider. Input frequency 'fin', output frequency 'fout' (less than "fin") with 50 % duty cycle. (Hint: you can use parameters and all).
-
-module tb;
+module testbench;
   reg fin,reset;
   wire fout;
  
   initial
     begin
     	$dumpfile("dump.vcd");
-    	$dumpvars(1, tb);
+      $dumpvars(1, testbench);
       	fin= 1'b0;
       	#5 reset=1'b1;
         #10 reset=1'b0;
@@ -19,9 +15,9 @@ module tb;
   always
         #5  fin=~fin; 
  
-  odd_div t1(fin,reset,fout);
+  frequency_divider m1(fin,reset,fout);
   
   initial
-    $monitor("clk=%b,reset=%b,clk_out=%b",fin,reset,fout);
+    $monitor("fin = %b, reset = %b, fout = %b",fin,reset,fout);
  
 endmodule
